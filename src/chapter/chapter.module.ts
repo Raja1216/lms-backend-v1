8e57c9101dec413ef29176275df35d21f5709d06
Subhajit121 <pubgraj.tornado1216@gmail.com>
2026-02-13 19:31:13 +0530

    Commited for course structure changes

--- src/chapter/chapter.module.ts
+++ src/chapter/chapter.module.ts
@@ -3,9 +3,10 @@ import { ChapterController } from './chapter.controller';
 import { ChapterService } from './chapter.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule,PrismaService],
+  imports: [AuthModule,PrismaModule],
   controllers: [ChapterController],
   providers: [ChapterService],
 })
--- src/module/module.module.ts
+++ src/module/module.module.ts
@@ -3,9 +3,10 @@ import { ModuleController } from './module.controller';
 import { ModuleService } from './module.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthModule } from 'src/auth/auth.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule,PrismaService],
+  imports: [AuthModule,PrismaModule],
   controllers: [ModuleController],
   providers: [ModuleService],
 })
