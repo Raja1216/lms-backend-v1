b3f6044a2c593173e021cae5b95313b27cd027e5
unknown <Raja1216>
2026-06-23 11:24:45 +0530

    Code commited for changes

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -155,7 +155,9 @@ export class CourseService {
     };
 
     if (!isAdmin) {
-      if (userType === 'STUDENT') {
+      if (isTeacher) {
+        // Teachers can see everything
+      } else if (userType === 'STUDENT') {
         whereClause.AND.push({
           OR: [
             {
@@ -805,7 +807,11 @@ export class CourseService {
     });
 
     try {
-      await this.activityLogService.logActivity(user.id, 'Payment Success', course.id);
+      await this.activityLogService.logActivity(
+        user.id,
+        'Payment Success',
+        course.id,
+      );
     } catch (err) {
       console.error('Failed to log Payment Success activity', err);
     }
